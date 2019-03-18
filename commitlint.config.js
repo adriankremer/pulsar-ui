fd57a7eeed30ae361404a9d50ec8cbd41073863c
Adrian Kremer <adrian.kremer@namics.com>
2019-03-18 12:21:18 +0100

    refactor: changed commitlint subject-case to start-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-case": [0, "always", "start-case"]
+    "subject-case": [0, "always", "start-case"]
   }
 };
