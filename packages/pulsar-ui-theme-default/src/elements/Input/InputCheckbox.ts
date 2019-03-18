6cc689ed326381cd9e39a8cc4590c92dcaa0588d
Adrian Kremer <adrian.kremer@namics.com>
2019-03-18 09:17:42 +0100

    fix: prettier jss

--- packages/pulsar-ui-theme-default/src/elements/Input/InputCheckbox.ts
+++ packages/pulsar-ui-theme-default/src/elements/Input/InputCheckbox.ts
@@ -1,4 +1,4 @@
-import { css } from "namics-ui";
+import { css } from "pulsar-ui";
 import { palette as p, theme } from "styled-tools";
 
 const InputCheckbox = css`
@@ -21,7 +21,14 @@ const InputCheckbox = css`
       background-position: center;
       background-size: .8rem;
       background-repeat: no-repeat;
-      background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path fill='none' d='M0 0h24v24H0z'/><path fill='${props => encodeURIComponent(props.theme.palette.white)}' d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg>");
+      background-image: url("data:image/svg+xml;utf8,\
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>\
+          <path fill='none' d='M0 0h24v24H0z'/>\
+          <path fill='\
+          ${props => encodeURIComponent(props.theme.palette.white)}\
+          ' d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/>\
+          </svg>\
+      ");
     }
   }
 `;
