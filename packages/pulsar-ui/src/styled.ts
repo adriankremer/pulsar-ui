775f8c8df8a6a8e21e7bdefe31c97ca90d190419
Adrian Kremer <adrian.kremer@namics.com>
2019-03-19 22:25:23 +0100

    refactor(core): removed styled-components, added emotion global

--- packages/pulsar-ui/src/styled.ts
+++ packages/pulsar-ui/src/styled.ts
@@ -1,27 +1,7 @@
-import {
-  keyframes,
-  createGlobalStyle,
-  isStyledComponent,
-  ThemeConsumer,
-  withTheme,
-  ServerStyleSheet,
-  StyleSheetManager
-} from "styled-components";
-
-import { css } from "@emotion/core";
+import { css, Global } from "@emotion/core";
 import { ThemeProvider } from "emotion-theming";
 import styled from "@emotion/styled";
 
-export {
-  css,
-  keyframes,
-  createGlobalStyle,
-  isStyledComponent,
-  ThemeConsumer,
-  ThemeProvider,
-  withTheme,
-  ServerStyleSheet,
-  StyleSheetManager
-};
+export { css, Global, ThemeProvider };
 
 export default styled;
