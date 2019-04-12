a137c3fed2480d228af92b443a3020826761cc46
Adrian Kremer <adrian.kremer@namics.com>
2019-04-12 08:56:54 +0200

    refactor: system optimizations

--- packages/system-default/src/Button.ts
+++ packages/system-default/src/Button.ts
@@ -1,10 +1,13 @@
 import { css, cx } from "emotion";
 
-export const useButtonProps = ({ system }, { className, ...htmlProps }) => {
+export function useButtonProps(
+  { system }: any,
+  { className, ...htmlProps }: any
+) {
   console.log(system);
   const button = css`
     background-color: salmon;
   `;
 
   return { ...htmlProps, className: cx(button, className) };
-};
+}
