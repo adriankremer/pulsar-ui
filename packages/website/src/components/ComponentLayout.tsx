7a1805be019e9b73bce04ddc53e7993e514e4349
Adrian Kremer <adrian.kremer@namics.com>
2019-04-17 17:38:04 +0200

    chore: ?!

--- packages/website/src/components/ComponentLayout.tsx
+++ packages/website/src/components/ComponentLayout.tsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Grid, GridCell, Provider } from "@pulsar-ui/core";
--- packages/website/src/components/layout.tsx
+++ packages/website/src/components/layout.tsx
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react";
 import { Provider } from "@pulsar-ui/core";
 import * as system from "@pulsar-ui/system";
