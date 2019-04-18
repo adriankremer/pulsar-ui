baa24e57f279ef9d12122b87db4b8898a383608a
Adrian Kremer <adrian.kremer@namics.com>
2019-04-18 09:09:29 +0200

    refactor: changed logo stage width

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -6,7 +6,7 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ width: `w00px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
   </Layout>
